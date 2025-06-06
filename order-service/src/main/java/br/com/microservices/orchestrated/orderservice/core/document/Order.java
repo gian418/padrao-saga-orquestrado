26a5cf647fda90e879674a55fec91c540e16088f
Giancarlo Haack Albano <gian418@gmail.com.br>
2025-06-06 18:10:53 -0300

    Iniciando criacao dos dtos

---
+++ order-service/src/main/java/br/com/microservices/orchestrated/orderservice/core/document/Order.java
@@ -0,0 +1,17 @@
+package br.com.microservices.orchestrated.orderservice.core.document;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.List;
+
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class Order {
+    private String id;
+    private List<OrderProducts> products;
+}
